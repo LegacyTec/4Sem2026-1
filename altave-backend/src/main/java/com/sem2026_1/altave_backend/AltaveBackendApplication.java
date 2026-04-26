1797eb5ee1f02db7955220a0ada92eb1d919740f
JohnatanCoelho <johnatansouza242@gmail.com>
2026-04-26 17:49:47 -0300

    feat: #32 backend criado com as dependências necessárias e docker file configurado para executar o banco oracle

---
+++ altave-backend/src/main/java/com/sem2026_1/altave_backend/AltaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sem2026_1.altave_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaveBackendApplication.class, args);
+	}
+
+}
---
+++ altave-backend/src/test/java/com/sem2026_1/altave_backend/AltaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sem2026_1.altave_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltaveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
